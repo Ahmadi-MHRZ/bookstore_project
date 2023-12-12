97a4342175c93de45241967bcf1e224e80f3646a
Ahmadi-MHRZ <mhrz.ahd20@gmail.com>
2023-12-12 17:59:37 +0330

    add BookCreteView

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.BookListview.as_view(), name='book_list'),
-    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail')
+    path('<int:pk>/', views.BookDetailView.as_view(), name='book_detail'),
+    path('new/', views.BookCreteView.as_view(), name='book_create'),
+    path('<int:pk>/edit/', views.BookUpdateView.as_view(), name='book_update'),
+
+
 
 ]
--- books/views.py
+++ books/views.py
@@ -12,3 +12,17 @@ class BookListview(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     template_name = 'books/book_detail.html'
+
+
+class BookCreteView(generic.CreateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_create.html'
+
+
+class BookUpdateView(generic.UpdateView):
+    model = Book
+    fields = ['title', 'author', 'description', 'price']
+    template_name = 'books/book_update.html'
+
+
